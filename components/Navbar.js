174f08f16fc244ff0e893ed1a2ad5d638cbdeddb
developer <150611229+fanlyai@users.noreply.github.com>
2023-11-15 17:43:58 +0300

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
@@ -8,10 +7,7 @@ import logo from "../public/assets/logo1.png";
 
 const outfit = Outfit({ weight: "400", subsets: ["latin"] });
 
-
 export default function Navbar() {
-
-
   return (
     <div className="z-10">
       <nav className="w-screen bg-[#0F0F0F] z-10  ">
@@ -21,43 +17,29 @@ export default function Navbar() {
               <div className="flex items-center">
                 <Image src={logo} width={200} />
               </div>
-
-              
             </div>
           </div>
           <div>
             <div className={outfit.className}>
-            <div
-            
-            >
-              <div className="items-center flex   justify-between md:flex space-x-6 md:space-y-0">
-                <div className="text-white text-base font-medium capitalize leading-relaxed">
-                <div
-                    className="cursor-pointer"
-                    
-                  >
-                    <Link href="https://whitepaper.fanlyai.com">Whitepaper</Link>
+              <div>
+                <div className="items-center flex   justify-between  space-x-6 md:space-y-0">
+                  <div className="text-white text-base font-medium capitalize leading-relaxed">
                     
 
+                    <div className="cursor-pointer">
+                      <Link href="https://presale.fanlyai.com">Presale</Link>
+                    </div>
                   </div>
-                  
-                  
-                  <div
-                    className="cursor-pointer"
-                    
-                  >
-                    <Link href="https://presale.fanlyai.com">Presale</Link>
-                    
-
-                  </div>
-                </div>
-                <div className="text-white cursor-pointer text-base">
-                 
+                  <div className="text-white cursor-pointer text-base">
                     <Link href="https://chat.fanlyai.com">Demo</Link>
-                 
-                </div> 
-               
-              </div></div>
+                  </div>
+                  <div className="cursor-pointer text-white ">
+                      <Link href="https://whitepaper.fanlyai.com">
+                        Whitepaper
+                      </Link>
+                    </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>

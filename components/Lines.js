e019cffac730bb751f576da8cb095fc9c8d6e175
developer <150611229+fanlyai@users.noreply.github.com>
2023-11-25 23:38:48 +0300

    lines added

--- components/Lines.js
+++ components/Lines.js
@@ -12,19 +12,23 @@ export default function Lines(){
             <div className="w-screen h-[4px] bg-white"></div>
             <div className="w-screen h-[5px] bg-white"></div>
             <div className="w-screen h-[6px] bg-white"></div>
+            <div className="w-screen h-[7px] bg-white"></div>
             <div className="w-screen h-[8px] bg-white"></div>
+            <div className="w-screen h-[9px] bg-white"></div>
             <div className="w-screen h-[10px] bg-white"></div>
             <div className="w-screen h-[12px] bg-white"></div>
             <div className="w-screen h-[14px] bg-white"></div>
             <div className="w-screen h-[16px] bg-white"></div>
             <div className="w-screen h-[18px] bg-white"></div>
+            <div className="w-screen h-[20px] bg-white"></div>
+            <div className="w-screen h-[22px] bg-white"></div>
             <div className="w-screen h-[24px] bg-white"></div>
             <div className="w-screen h-[30px] bg-white"></div>
             <div className="w-screen h-[36px] bg-white"></div>
-            <div className="w-screen h-[42px] bg-white"></div>
-            <div className="w-screen h-[48px] bg-white"></div>
-            <div className="w-screen h-[60px] bg-white"></div>
-            <div className="w-screen h-[80px] bg-white"></div>
+            <div className="w-screen h-[40px] bg-white"></div>
+            <div className="w-screen h-[46px] bg-white"></div>
+            <div className="w-screen h-[52px] bg-white"></div>
+            <div className="w-screen h-[56px] bg-white"></div>
             
             <div className="w-screen h-[100px] bg-white"></div>
         </div>
